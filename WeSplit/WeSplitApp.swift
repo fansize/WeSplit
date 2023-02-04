7efb2a82206b3c663bd05d29f49fdfb731a9f72f
fansize <langlang_1991@126.com>
2023-02-04 15:59:59 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 唐浪 on 2023/2/4.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

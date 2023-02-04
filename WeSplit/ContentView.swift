f39c34ded39728da24bed8d0a7bffd048763f66e
fansize <langlang_1991@126.com>
2023-02-04 23:30:01 +0800

    Finish Project WeSplit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,19 +8,133 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerson: Double {
+        // calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    // MARK: Input Amount
+                    TextField(
+                        value: $checkAmount,
+                        format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                    ){
+                        Text("Amount")
+                    }
+                    .keyboardType(.decimalPad)
+                    
+                    // MARK: Select number of people
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    // MARK: Select tip percentage
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        Text("This is the detail view")
+    }
+}
+
+struct Navigation2: View {
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    NavigationLink("Detail View") {
+                        DetailView()
+                    }
+                }
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct TestView2: View {
+    @State var tapCount = 0
+    
+    var body: some View {
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
+        }
+    }
+}
+
+struct InputView2: View {
+    @State private var name = ""
+    
+    var body: some View {
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Your name is \(name)")
+        }
+    }
+}
+
+struct PickView2: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) { name in
+                        Text(name)
+                    }
+                }
+            }
+            .navigationTitle("Select Student")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }

bf86ebdc4c196ecdba16cee71c3c662f4465548f
fansize <langlang_1991@126.com>
2023-02-06 19:41:37 +0800

    create project3 files

---
+++ WeSplit/ViewsAndModifiers.swift
@@ -0,0 +1,20 @@
+//
+//  ViewsAndModifiers.swift
+//  WeSplit
+//
+//  Created by 唐浪 on 2023/2/6.
+//
+
+import SwiftUI
+
+struct ViewsAndModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewsAndModifiers_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewsAndModifiers()
+    }
+}

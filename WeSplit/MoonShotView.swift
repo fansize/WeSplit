b71a86a444ba4206390b7fb5bfe5528bd4e50da6
TangLang <langlang_1991@126.com>
2023-02-20 23:08:22 +0800

    done challenge

--- WeSplit/MoonShotView.swift
+++ WeSplit/MoonShotView.swift
@@ -11,47 +11,26 @@ struct MoonShotView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
-    let columns = [
-        GridItem(.adaptive(minimum: 150))
-    ]
+    @State private var showingGrid = true
+    
+    
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formattedLaunchedDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                            }
-                            .padding(.vertical)
-                            .frame(maxWidth: .infinity)
-                            .backgroundStyle(.lightBackground)
-                        }
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.lightBackground)
-                        )
-                        .padding()
-                    }
+            Group {
+                if showingGrid {
+                    GridLayout(missions: missions, astronauts: astronauts)
+                } else {
+                    ListLayout(missions: missions, astronauts: astronauts)
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                Button(showingGrid ? "List" : "Grid") {
+                    showingGrid.toggle()
+                }
+            })
             .background(.darkBackground)
             .preferredColorScheme(.dark)
         }
@@ -61,5 +40,84 @@ struct MoonShotView: View {
 struct MoonShotView_Previews: PreviewProvider {
     static var previews: some View {
         MoonShotView()
+    }
+}
+
+struct GridLayout: View {
+    let missions: [Mission]
+    let astronauts: [String: Astronaut]
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Text(mission.formattedLaunchedDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.5))
+                            }
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .backgroundStyle(.lightBackground)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
+                    .padding()
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct ListLayout: View {
+    let missions: [Mission]
+    let astronauts: [String: Astronaut]
+    
+    var body: some View {
+        List {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    HStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .padding(.horizontal)
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            Text(mission.formattedLaunchedDate)
+                                .font(.caption)
+                                .foregroundColor(.white.opacity(0.5))
+                        }
+                    }
+                }
+            }
+            .listRowBackground(Color.darkBackground)
+        }
+        .listStyle(.plain)
     }
 }
--- WeSplit/PageListView.swift
+++ WeSplit/PageListView.swift
@@ -9,45 +9,16 @@ import SwiftUI
 
 struct PageListView: View {
     var body: some View {
-        let layout = [
-            GridItem(.adaptive(minimum: 8, maximum: 120))
-        ]
-        
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
+        NavigationStack {
+            Form {
+                NavigationLink("WeSplit", destination: WeSplitView())
+                NavigationLink("GuessTheFlag", destination: GuessFlagView())
+                NavigationLink("BetterReset", destination: BetterResetView())
+                NavigationLink("WordScramble", destination: WorldScrambleView())
+                NavigationLink("iExpence", destination: IExpenceView())
+                NavigationLink("MoonShot", destination: MoonShotView())
             }
-        }
-        
-    }
-    
-    func decodeJson() {
-        let input = """
-        {
-            "name": "Taylor Swift",
-            "address": {
-                "street": "555, Taylor Swift Avenue",
-                "city": "Nashville"
-            }
-        }
-        """
-        
-        struct User: Codable {
-            let name: String
-            let address: Address
-        }
-        
-        struct Address: Codable {
-            let street: String
-            let city: String
-        }
-        
-        let data = Data(input.utf8)
-        let decode = JSONDecoder()
-        if let user = try? decode.decode(User.self, from: data) {
-            print(user.address.street)
+            .navigationTitle("HackSwift Practice")
         }
     }
 }

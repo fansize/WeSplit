522cc3d7f2520b0a5fade778958b7319f9a44344
TangLang <langlang_1991@126.com>
2023-02-24 13:21:19 +0800

    add bookworm

---
+++ WeSplit/Bookworm/BookwormView.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormView.swift
+//  WeSplit
+//
+//  Created by 唐浪 on 2023/2/24.
+//
+
+import SwiftUI
+
+struct BookwormView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct BookwormView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookwormView()
+    }
+}
